9113435b37a621d7aa75158ccd1d6595b6ef72a6
HishamLadha <hishamladha@gmail.com>
2023-07-27 23:51:35 -0400

    Added Tv-details JS

--- js/script.js
+++ js/script.js
@@ -76,6 +76,9 @@ async function displayMovieDetails(){
 
     const movie = await fetchAPIData(`movie/${movieId}`);
 
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
 
     div.innerHTML = `<div class="details-top">
@@ -129,6 +132,93 @@ async function displayMovieDetails(){
   document.querySelector('#movie-details').appendChild(div);
 }
 
+
+// Display movie details
+async function displayShowDetails(){
+    const showId = window.location.search.split('=')[1];
+
+    const show = await fetchAPIData(`tv/${showId}`);
+
+    // Overlay for background image
+    displayBackgroundImage('show', show.backdrop_path);
+
+    const div = document.createElement('div');
+
+    div.innerHTML = `<div class="details-top">
+    <div>
+    ${
+        // Ternary if statement showing image if it is there and if not, show no image icon
+        show.poster_path?`<img
+        src="https://images.tmdb.org/t/p/w500${show.poster_path}"
+        class="card-img-top"
+        alt="${show.name}"
+      />` : `<img
+      src="images/no-image.jpg"
+      class="card-img-top"
+      alt="${show.name}"
+    />`
+    }
+    </div>
+    <div>
+      <h2>${show.name}</h2>
+      <p>
+        <i class="fas fa-star text-primary"></i>
+        ${show.vote_average.toFixed(1)} / 10
+      </p>
+      <p class="text-muted">Release Date: ${show.first_air_date}</p>
+      <p>
+        ${show.overview}
+      </p>
+      <h5>Genres</h5>
+      <ul class="list-group">
+        ${show.genres.map((genre) => `<li>${genre.name}</li>`).join('')}
+      </ul>
+      <a href="${show.homepage}" target="_blank" class="btn">Visit Show Homepage</a>
+    </div>
+  </div>
+  <div class="details-bottom">
+    <h2>Show Info</h2>
+    <ul>
+      <li><span class="text-secondary">Number Of Episodes:</span> ${show.number_of_episodes}</li>
+      <li>
+        <span class="text-secondary">Last Episode To Air:</span> ${show.last_episode_to_air.name}
+      </li>
+      <li><span class="text-secondary">Status:</span> ${show.status}</li>
+    </ul>
+    <h4>Production Companies</h4>
+    <div class="list-group">
+        ${show.production_companies.map((company) => `<span>${company.name}</span>`).join(', ')}
+    </div>
+  </div>`;
+
+  document.querySelector('#show-details').appendChild(div);
+}
+
+
+// Display Backdrop on details page
+
+function displayBackgroundImage(type, backgroundPath){
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroundPath})`;
+    overlayDiv.style.backgroundPosition = 'center'; 
+    overlayDiv.style.backgroundRepeat = 'no-repeat'; 
+    overlayDiv.style.height = '100vh'; 
+    overlayDiv.style.width = '100vw'; 
+    overlayDiv.style.position = 'absolute'; 
+    overlayDiv.style.top = '0'; 
+    overlayDiv.style.left = '0'; 
+    overlayDiv.style.zIndex = '-1'; 
+    overlayDiv.style.opacity = '0.1';
+
+    if(type === 'movie'){
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    }
+    else{
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
+}
+
+
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint){
     const API_KEY = '254c8da735e5e272fb7eaa7b1019fd61';
@@ -180,7 +270,7 @@ function init(){
             displayMovieDetails();
             break;
         case '/Projects/flixx-app/tv-details.html':
-            console.log('TV Details');
+            displayShowDetails();
             break;
         case '/Projects/flixx-app/search.html':
             console.log('Search');
