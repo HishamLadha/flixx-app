f06cfab52b2bc3f667cd666b33928279fc71b108
HishamLadha <hishamladha@gmail.com>
2023-07-29 00:53:49 -0400

    Added search, pagination and fixed bugs. Project Complete.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
 const global = {
     currentPage: window.location.pathname,
+    search: {
+      term: '',
+      type: '',
+      page: 1,
+      totalPages: 1,
+      totalResults: 0
+    },
+    api: {
+      apiKey: '254c8da735e5e272fb7eaa7b1019fd61',
+      apiURL: 'https://api.themoviedb.org/3/'
+    }
 }
 
 // Display 20 most popular movies
@@ -33,7 +44,7 @@ async function displayPopularMovies() {
         `;
 
         document.querySelector('#popular-movies').appendChild(div);
-    })
+    });
 }
 
 // Display 20 most popular tv shows
@@ -67,13 +78,12 @@ async function displayPopularShows() {
         `;
 
         document.querySelector('#popular-shows').appendChild(div);
-    })
+    });
 }
 
 // Display movie details
 async function displayMovieDetails(){
     const movieId = window.location.search.split('=')[1];
-
     const movie = await fetchAPIData(`movie/${movieId}`);
 
     // Overlay for background image
@@ -108,9 +118,9 @@ async function displayMovieDetails(){
       </p>
       <h5>Genres</h5>
       <ul class="list-group">
-        <li>${movie.genres[0].name}</li>
-        <li>${movie.genres[1].name}</li>
-        <li>${movie.genres[2].name}</li>
+        ${movie.genres.length === 0 ? '<li></li>' : movie.genres.map((genre) => {
+          return `<li>${genre.name}</li>`;
+        }).join('')}
       </ul>
       <a href="${movie.homepage}" target="_blank" class="btn">Visit Movie Homepage</a>
     </div>
@@ -221,8 +231,8 @@ function displayBackgroundImage(type, backgroundPath){
 
 // Fetch data from TMDB API
 async function fetchAPIData(endpoint){
-    const API_KEY = '254c8da735e5e272fb7eaa7b1019fd61';
-    const API_URL = 'https://api.themoviedb.org/3/';
+    const API_KEY = global.api.apiKey;
+    const API_URL = global.api.apiURL;
 
     showSpinner();
 
@@ -235,6 +245,21 @@ async function fetchAPIData(endpoint){
     return data;
 }
 
+// Make request to serach
+async function searchAPIData(endpoint){
+  const API_KEY = global.api.apiKey;
+  const API_URL = global.api.apiURL;
+
+  showSpinner();
+
+  const response = await fetch(`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
+  const data = await response.json();
+
+  hideSpinner();
+
+  return data;
+}
+
 // Show spinner function on every page
 
 function showSpinner(){
@@ -244,6 +269,133 @@ function showSpinner(){
 function hideSpinner(){
     document.querySelector('.spinner').classList.remove('show');
 }
+
+// Search Movies/Shows
+
+async function search(){
+  const queryString = window.location.search;
+  // const type = queryString[0].split('=')[1];
+  // const movieName = queryString[1].split('=')[1];
+  const urlParams = new URLSearchParams(queryString);
+
+  global.search.type = urlParams.get('type');
+  global.search.term = urlParams.get('search-term');
+
+  if(global.search.term !== '' && global.search.term !== null){
+    const {results, total_pages, page, total_results} = await searchAPIData();
+
+    global.search.page = page;
+    global.search.totalPages = total_pages;
+    global.search.totalResults = total_results;
+
+    
+    if(results.length === 0){
+      showAlert('No results found');
+      return;
+    }
+    displaySearchResults(results);
+
+    document.querySelector('#search-term').value = '';
+
+  }else{
+    showAlert('Please enter a search term');
+  }
+
+
+}
+
+function displaySearchResults(results){
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
+  results.forEach(result => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+      <a href="/Projects/flixx-app/${global.search.type}-details.html?id=${result.id}">
+        ${
+            // Ternary if statement showing image if it is there and if not, show no image icon
+            result.poster_path?`<img
+            src="https://images.tmdb.org/t/p/w500/${result.poster_path}"
+            class="card-img-top"
+            alt="${global.search.type === 'movie' ? result.title : result.name}"
+          />` : `<img
+          src="images/no-image.jpg"
+          class="card-img-top"
+          alt="${global.search.type === 'movie' ? result.title : result.name}"
+        />`
+        }
+      </a>
+      <div class="card-body">
+        <h5 class="card-title">${global.search.type === 'movie' ? result.title : result.name}</h5>
+        <p class="card-text">
+          <small class="text-muted">Release: ${global.search.type === 'movie' ? result.release_date : result.first_air_date}</small>
+        </p>
+      </div>
+    `;
+
+    document.querySelector('#search-results-heading').innerHTML = `
+        <h2>${results.length} of ${global.search.totalResults} Results for the ${global.search.term}</h2>
+    `;
+
+    document.querySelector('#search-results').appendChild(div);
+});
+
+displayPagination();
+}
+
+// Create and display pagination for search
+function displayPagination(){
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+    <button class="btn btn-primary" id="prev">Prev</button>
+    <button class="btn btn-primary" id="next">Next</button>
+    <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable prev button if on first page
+  if(global.search.page === 1){
+    document.querySelector('#prev').disabled = true;
+  }
+  
+  // Disable next button if on last page
+  if(global.search.page === global.search.totalPages){
+    document.querySelector('#next').disabled = true;
+  }
+
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const {results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const {results, total_pages} = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+}
+
+// Show Alert
+function showAlert(message, className = 'alert-error'){
+  const alertEl = document.createElement('div');
+  alertEl.classList.add('alert', className);
+  alertEl.appendChild(document.createTextNode(message));
+  document.querySelector('#alert').appendChild(alertEl);
+
+  setTimeout(() => alertEl.remove(), 3000);
+
+}
+
 
 // Display slider movies
 async function displaySlider(){
@@ -323,7 +475,7 @@ function init(){
             displayShowDetails();
             break;
         case '/Projects/flixx-app/search.html':
-            console.log('Search');
+            search();
             break;
 
     }
